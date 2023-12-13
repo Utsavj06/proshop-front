36d72670dae9df9b4fbb3d47c546a1ed72a7f2c1
Utsavj06 <85385280+Utsavj06@users.noreply.github.com>
2023-12-13 18:34:54 +0530

    Update $ to ₹

--- src/screens/PlaceOrderScreen.jsx
+++ src/screens/PlaceOrderScreen.jsx
@@ -130,25 +130,25 @@ const PlaceOrderScreen = () => {
                   <ListGroup.Item>
                     <Row>
                       <Col>Items</Col>
-                      <Col>${cart.itemsPrice}</Col>
+                      <Col>₹{cart.itemsPrice}</Col>
                     </Row>
                   </ListGroup.Item>
                   <ListGroup.Item>
                     <Row>
                       <Col>Shipping</Col>
-                      <Col>${cart.shippingPrice}</Col>
+                      <Col>₹{cart.shippingPrice}</Col>
                     </Row>
                   </ListGroup.Item>
                   <ListGroup.Item>
                     <Row>
                       <Col>Tax</Col>
-                      <Col>${cart.taxPrice}</Col>
+                      <Col>₹{cart.taxPrice}</Col>
                     </Row>
                   </ListGroup.Item>
                   <ListGroup.Item>
                     <Row>
                       <Col>Total</Col>
-                      <Col>${cart.totalPrice}</Col>
+                      <Col>₹{cart.totalPrice}</Col>
                     </Row>
                   </ListGroup.Item>
                   <ListGroup.Item>
