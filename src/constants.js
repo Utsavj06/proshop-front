bc2b0ec69e75c1476a71c888f5e59dcf1703fa64
Utsavj06 <85385280+Utsavj06@users.noreply.github.com>
2023-12-14 13:41:17 +0530

    Refactor Code

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,7 @@
-// export const BASE_URL = process.env.NODE_ENV === 'development'? 'http://localhost:5000' : '';
 // export const BASE_URL = 'http://localhost:5000/';
-// export const BASE_URL = '';
 export const BASE_URL = 'https://proshop-back.onrender.com/';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
 export const ADD_PRODUCT_URL = '/api/addProduct';
 export const stripeApiKey = 'pk_test_51N2bqvSIEzY742rCaQ3y1M91nSKmitoa3HOsUSul3FIXMuK05wIfNoAYELsqIdcPH9dUXrCj4qrnz0ECGphgbD3E0008FjK9LM'
