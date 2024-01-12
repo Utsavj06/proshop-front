8eafbe7b6408fee2bd528eca76b2cb90655615ed
Utsavj06 <85385280+Utsavj06@users.noreply.github.com>
2024-01-12 17:28:13 +0530

    Update Product.jsx for shimmer ui

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,11 +4,29 @@ import { Link } from "react-router-dom";
 import Rating from "./Rating";
 
 const Product = ({ product }) => {
+  if (!product) {
+    return (
+      <Card className="my-3 p-3 rounded">
+        <Link>
+          <div style={{ height: "200px", width: "100%", backgroundImage: "linear-gradient(to right, #cbc0c0, white)" }} />
+        </Link>
+        <Card.Body>
+          <Link>
+            <div style={{ width: "100%", backgroundImage: "linear-gradient(to right, #cbc0c0, white)", height: "2.5em", marginBottom: "10px" }} />
+          </Link>
+
+          <div style={{ width: "100%", backgroundImage: "linear-gradient(to right, #cbc0c0, white)", height: "20px" }}></div>
+          <div style={{ width: "100%", backgroundImage: "linear-gradient(to right, #cbc0c0, white)", height: "30px", marginTop: "10px" }}></div>
+        </Card.Body>
+      </Card>
+    )
+  }
+
   return (
     <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
         {/* <Card.Img src={product.image} variant="top" /> */}
-        <img src={product.image} style={{height:'200px', width:'100%'}} />
+        <img src={product.image} style={{ height: "200px", width: "100%" }} alt={product.name} />
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
@@ -16,7 +34,6 @@ const Product = ({ product }) => {
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
-
         <Card.Text as="div">
           <Rating value={product.rating} text={`${product.numReviews} reviews`} />
         </Card.Text>
