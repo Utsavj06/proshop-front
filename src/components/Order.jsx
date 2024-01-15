da48941103a5ec0d8842bff062e25b5a44d5f600
Utsavj06 <85385280+Utsavj06@users.noreply.github.com>
2024-01-15 13:04:21 +0530

    Create Order Page for Delivering Agent

---
+++ src/components/Order.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Order = ({ product }) => {
+  if (!product) {
+    return (
+      <Card className="my-3 p-3 rounded">
+        <Link>
+          <div style={{ height: "200px", width: "100%", backgroundImage: "linear-gradient(to right, #cbc0c0, white)" }} />
+        </Link>
+        <Card.Body>
+          <Link>
+            <div style={{ width: "100%", backgroundImage: "linear-gradient(to right, #cbc0c0, white)", height: "2.5em", marginBottom: "10px" }} />
+          </Link>
+
+          <div style={{ width: "100%", backgroundImage: "linear-gradient(to right, #cbc0c0, white)", height: "20px" }}></div>
+          <div style={{ width: "100%", backgroundImage: "linear-gradient(to right, #cbc0c0, white)", height: "30px", marginTop: "10px" }}></div>
+        </Card.Body>
+      </Card>
+    )
+  }
+
+  return (
+    <Card className="my-3 p-3 rounded">
+      <Link to={`/product/${product._id}`}>
+        <img src={product.orderItems[0].image} style={{ height: "200px", width: "100%" }} alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div" className="product-title">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+        <Card.Text as="div">
+          <p>{`is Delivered? : ${product.isDelivered}`}</p>
+          <p>{`is Paid? : ${product.isPaid}`}</p>
+        </Card.Text>
+        <Card.Text as="p"><b>{`Total Price: ₹ ${product.totalPrice}`}</b></Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Order
