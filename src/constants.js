e911aa0243c94f7068fa2b3e0bf7ff9b75d58d80
Utsavj06 <85385280+Utsavj06@users.noreply.github.com>
2024-01-16 14:50:57 +0530

    Update constants.js to add new api route for delivery

--- src/constants.js
+++ src/constants.js
@@ -4,4 +4,5 @@ export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
 export const ADD_PRODUCT_URL = '/api/addProduct';
+export const DELIVERY_URL = '/api/delivery';
 export const stripeApiKey = 'pk_test_51N2bqvSIEzY742rCaQ3y1M91nSKmitoa3HOsUSul3FIXMuK05wIfNoAYELsqIdcPH9dUXrCj4qrnz0ECGphgbD3E0008FjK9LM'
