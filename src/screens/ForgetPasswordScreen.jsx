bd75edb5a1c0bc27ac5150d10ba0cbb3d2dc5d9e
Utsavj06 <85385280+Utsavj06@users.noreply.github.com>
2024-03-11 16:24:23 +0530

    add ForgetPasswordScreen.jsx

---
+++ src/screens/ForgetPasswordScreen.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import { Button, Col, Form, Row } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
+import FormContainer from '../components/FormContainer';
+
+const ForgetPasswordScreen = () => {
+  const [password, setPassword] = useState("");
+  const [cPass, setCPass] = useState("");
+
+  const navigate = useNavigate();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const { search } = useLocation();
+  console.log(search)
+  const sp = new URLSearchParams(search);
+  console.log(sp)
+  const email = sp.get("email") || "/";
+  const token = sp.get("token");
+  console.log(email, token)
+
+  const submitHandler = async (e) => {
+
+  };
+
+  return (
+    <>
+      <FormContainer>
+        <h1>Really want to reset your password</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="my-2" controlId="email">
+            <Form.Label>Enter Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className="my-2" controlId="password">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm password"
+              value={cPass}
+              onChange={(e) => setCPass(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <br />
+          <Button type="submit" variant="primary">
+            Reset the Password
+          </Button>
+        </Form>
+      </FormContainer>
+    </>
+  );
+}
+
+export default ForgetPasswordScreen
